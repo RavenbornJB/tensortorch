7406e94f128634f53a35f06702ec76b23493a719
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-24 23:16:18 +0200

    Finalized layer API, added utility files

--- inc/layer.h
+++ inc/layer.h
@@ -7,27 +7,39 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include <random>
 #include <stdexcept>
 
+#include "linalg.h"
+#include "utility.h"
 
 class Layer {
 private:
-    std::vector<std::vector<double>> W;
-    std::vector<double> b;
+    Matrix<double> W;
+    Matrix<double> b;
+    Matrix<double> A_prev;
+    Matrix<double> Z;
+    size_t m;
+    double learning_rate;
 
-    std::vector<std::vector<double>> linear(const std::vector<std::vector<double>> &input);
-    static std::vector<std::vector<double>> sigmoid(const std::vector<std::vector<double>> &input);
-    static std::vector<std::vector<double>> tanh(const std::vector<std::vector<double>> &input);
-    static std::vector<std::vector<double>> relu(const std::vector<std::vector<double>> &input);
-    std::vector<std::vector<double>>(*activation)(const std::vector<std::vector<double>> &); // TODO try with std::function
+    Matrix<double> linear(const Matrix<double> &input);
+    static Matrix<double> sigmoid(const Matrix<double> &input);
+    static Matrix<double> tanh(const Matrix<double> &input);
+    static Matrix<double> relu(const Matrix<double> &input);
+    Matrix<double>(*activation)(const Matrix<double> &); // TODO try with std::function
+
+    std::vector<Matrix<double>> linear_backward(const Matrix<double> &dZ);
+    static Matrix<double> sigmoid_backward(const Matrix<double> &dA, const Matrix<double> &Z);
+    static Matrix<double> tanh_backward(const Matrix<double> &dA, const Matrix<double> &Z);
+    static Matrix<double> relu_backward(const Matrix<double> &dA, const Matrix<double> &Z);
+    Matrix<double>(*activation_backward)(const Matrix<double> &, const Matrix<double> &);
 
 public:
-    explicit Layer(const std::string& activation_type, size_t from_size, size_t to_size);
+    explicit Layer(const std::string& activation_type, size_t from_size, size_t to_size, double learning_rate);
     void print_parameters();
-    std::vector<std::vector<double>> forward(const std::vector<std::vector<double>> &input);
-    std::vector<std::vector<double>> backward(const std::vector<std::vector<double>> &dA); // TODO зробити
+    Matrix<double> forward(const Matrix<double> &input);
+    std::vector<Matrix<double>> backward(const Matrix<double> &dZ);
+    void update_parameters(const Matrix<double> &dW, const Matrix<double> &db);
 };
 
 
--- src/layer.cpp
+++ src/layer.cpp
@@ -4,112 +4,155 @@
 
 #include "layer.h"
 
-/* Тут визначається тип функції активації, а тоді ініціалізуються параметри W та b.
- * W ініціалізується випадковими числами з нормального розподілу (mean = 0, var = 2 / from_size) за методом
- * He initialization.
- * b ініціалізується нулями.
+/* This initializer fills W with randomly distributed values from N(mean=0, var=2/from_size)
+ * It also chooses the activation functions based on the type.
  * */
-Layer::Layer(const std::string& activation_type, size_t from_size, size_t to_size) {
-    if (activation_type == "sigmoid") {
-        this->activation = Layer::sigmoid;
-    } else if (activation_type == "tanh") {
-        this->activation = Layer::tanh;
-    } else if (activation_type == "relu") {
-        this->activation = Layer::relu;
-    } else {
-        throw std::logic_error("Activation type not allowed");
-    }
+Layer::Layer(const std::string& activation_type, size_t from_size, size_t to_size, double learning_rate)
+: b(*new Matrix<double>(to_size, 1, 0)), learning_rate(learning_rate), m(0) {
 
+    W = *new Matrix<double>(to_size, from_size, 0);
     std::default_random_engine gen{static_cast<long unsigned int>(time(nullptr))};
     std::normal_distribution<double> dist(0., std::sqrt(2. / from_size)); // He initialization
     for (size_t i = 0; i < to_size; ++i) {
-        std::vector<double> row;
-        row.reserve(from_size);
         for (size_t j = 0; j < from_size; ++j) {
-            row.push_back(dist(gen));
+            W(i, j) = dist(gen);
         }
-        W.push_back(row);
-        b.push_back(0);
+    }
+
+    if (activation_type == "sigmoid") {
+        activation = Layer::sigmoid;
+        activation_backward = Layer::sigmoid_backward;
+    } else if (activation_type == "tanh") {
+        activation = Layer::tanh;
+        activation_backward = Layer::tanh_backward;
+    } else if (activation_type == "relu") {
+        activation = Layer::relu;
+        activation_backward = Layer::relu_backward;
+    } else {
+        throw std::logic_error("Activation type not allowed");
     }
 }
 
-/* Дебаг-функція для друку параметрів
+/* Debug function for printing W and b.
  */
 void Layer::print_parameters() {
     std::cout << "W: " << std::endl;
-    for (auto & row : W) {
-        for (double el : row) std::cout << el << " ";
-        std::cout << std::endl;
-    }
-
+    W.print();
     std::cout << "\nb: " << std::endl;
-    for (double el: b) std::cout << el << "\n";
-    std::cout << std::endl;
+    b.print();
 }
 
-/* Плейсхолдери для функцій активації
- * TODO визначитись, чи будуть вони тут, чи в linalg
+/* Activation functions.
  */
-std::vector<std::vector<double>> Layer::sigmoid(const std::vector<std::vector<double>> &input) {
-    std::vector<std::vector<double>> output;
-    for (int i = 0; i < input.size(); ++i) {
-        std::vector<double> row;
-        row.reserve(input[0].size());
-        for (int j = 0; j < input[0].size(); ++j) {
-            row.push_back(1 / (1 + std::exp(- input[i][j])));
+Matrix<double> Layer::sigmoid(const Matrix<double> &input) {
+    Matrix<double> output(input.get_rows(), input.get_cols(), 0);
+    for (int i = 0; i < output.get_rows(); ++i) {
+        for (int j = 0; j < output.get_cols(); ++j) {
+            output(i, j) = 1 / (1 + std::exp(input(i, j)));
         }
-        output.push_back(row);
     }
     return output;
 }
-std::vector<std::vector<double>> Layer::tanh(const std::vector<std::vector<double>> &input) {
-    std::vector<std::vector<double>> output;
-    for (int i = 0; i < input.size(); ++i) {
-        std::vector<double> row;
-        row.reserve(input[0].size());
-        for (int j = 0; j < input[0].size(); ++j) {
-            row.push_back(std::tanh(input[i][j]));
+Matrix<double> Layer::tanh(const Matrix<double> &input) {
+    Matrix<double> output(input.get_rows(), input.get_cols(), 0);
+    for (int i = 0; i < output.get_rows(); ++i) {
+        for (int j = 0; j < output.get_cols(); ++j) {
+            output(i, j) = std::tanh(input(i, j));
         }
-        output.push_back(row);
     }
     return output;
 }
-std::vector<std::vector<double>> Layer::relu(const std::vector<std::vector<double>> &input) {
-    std::vector<std::vector<double>> output;
-    for (int i = 0; i < input.size(); ++i) {
-        std::vector<double> row;
-        row.reserve(input[0].size());
-        for (int j = 0; j < input[0].size(); ++j) {
-            row.push_back(input[i][j] * (int)(input[i][j] > 0));
+Matrix<double> Layer::relu(const Matrix<double> &input) {
+    Matrix<double> output(input.get_rows(), input.get_cols(), 0);
+    for (int i = 0; i < output.get_rows(); ++i) {
+        for (int j = 0; j < output.get_cols(); ++j) {
+            output(i, j) = input(i, j) * (int)(input(i, j) > 0);
         }
-        output.push_back(row);
     }
     return output;
 }
 
-/* Функція лінійного кроку: Z = W * X + b
- * TODO реалізувати через функції з linalg, коли вони будуть готові
+/* Linear forward propagation function: Z = W * X + b
+ *
  */
-std::vector<std::vector<double>> Layer::linear(const std::vector<std::vector<double>> &input) {
-    return input;
+Matrix<double> Layer::linear(const Matrix<double> &input) {
+    return dot(W, input) + b;
 }
 
-/* Функція активації
- * викликає один з варіантів активації:
- * sigmoid, tanh, relu (залежно від параметра в конструкторі)
+/* Forward-passes the input, first through the linear step,
+ * then through the activation function.
  */
+Matrix<double> Layer::forward(const Matrix<double> &input) {
+    A_prev = input;
+    m = input.get_cols();
+    Z = linear(input);
+    return activation(Z);
+}
 
-std::vector<std::vector<double>> Layer::forward(const std::vector<std::vector<double>> &input) {
-    return this->activation(this->linear(input));
+/* Backward activation functions.
+ * */
+Matrix<double> Layer::sigmoid_backward(const Matrix<double> &dA, const Matrix<double> &Z) {
+    Matrix<double> ones (Z.get_rows(), Z.get_cols(), 1);
+    return dA * (Z * (ones - Z));
+}
+Matrix<double> Layer::tanh_backward(const Matrix<double> &dA, const Matrix<double> &Z) {
+    Matrix<double> res(dA);
+    for (int i = 0; i < Z.get_rows(); ++i) {
+        for (int j = 0; j < Z.get_cols(); ++j) res(i, j) *= 1 / std::pow(std::cosh(Z(i, j)), 2);
+    }
+    return res;
+}
+Matrix<double> Layer::relu_backward(const Matrix<double> &dA, const Matrix<double> &Z) {
+    Matrix<double> res(dA);
+    for (int i = 0; i < Z.get_rows(); ++i) {
+        for (int j = 0; j < Z.get_cols(); ++j) res(i, j) *= (int)(Z(i, j) > 0);
+    }
+    return res;
+}
+
+/* Linear backpropagation function.
+ * Calculates dW, db (for gradient descent), and dA_prev (for previous layer).
+ */
+std::vector<Matrix<double>> Layer::linear_backward(const Matrix<double> &dZ) {
+    Matrix<double> dW = dot(dZ, transpose(A_prev)) / m;
+    Matrix<double> db = sum(dZ, 1) / m;
+    Matrix<double> dA_prev = dot(transpose(W), dZ);
+    return {dW, db, dA_prev};
+}
+
+/* Backward-passes the dA, first through the backward activation,
+ * then through the linear step.
+ * Calculates dW, db (for gradient descent), and dA_prev (for previous layer).
+ */
+std::vector<Matrix<double>> Layer::backward(const Matrix<double> &dA) {
+    Matrix<double> dZ = activation_backward(dA, Z);
+    return linear_backward(dZ);
+}
+
+/* Updates W and b based on dW and db according to gradient descent.
+ * By default, uses W/b -= learning_rate * dW/b
+ */
+void Layer::update_parameters(const Matrix<double> &dW, const Matrix<double> &db) {
+    // TODO update in-place when -= is shipped in linalg
+    W = W - dW * learning_rate;
+    b = b - db * learning_rate;
 }
 
 //int main() {
-//    Layer test_layer("relu", 5, 3);
-//    auto res = test_layer.forward({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {-1, -2, -3}, {-4, -5, -6}});
-//    for (const auto& el: res) {
-//        for (auto e: el) {
-//            std::cout << e << " ";
-//        }
-//        std::cout << std::endl;
+//    Layer test_layer("tanh", 5, 3, 0.01);
+//    std::vector<std::vector<double>> inp_forw = {{.1, .1, .1}, {.2, .2, .2}, {.3, .3, .3}, {.4, .4, .4}, {.5, .5, .5}};
+//    std::vector<std::vector<double>> inp_back = {{.2, .2, .2}, {.3, .3, .3}, {.4, .4, .4}};
+//    Matrix<double> in_f(inp_forw);
+//    Matrix<double> in_b(inp_back);
+//    auto out_f = test_layer.forward(in_f);
+//    auto out_b = test_layer.backward(in_b);
+//    std::cout << "dW, db, and dA_prev" << std::endl;
+//    for (const auto& matrix: out_b) {
+//        matrix.print();
 //    }
+//    std::cout << "W and b before update" << std::endl;
+//    test_layer.print_parameters();
+//    test_layer.update_parameters(out_b[0], out_b[1]);
+//    std::cout << "W and b after update" << std::endl;
+//    test_layer.print_parameters();
 //}
