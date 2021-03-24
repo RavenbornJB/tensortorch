017661bf070d7afb406caa00c09f458bcf814563
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-25 00:58:58 +0200

    Added parallel_linalg features with minor changes

--- inc/linalg.h
+++ inc/linalg.h
@@ -2,6 +2,18 @@
 #define NN_PROJECT_LINALG_H
 
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+#define THREADS_NUM 4
+
+struct matrix_ranges {
+    size_t r1;
+    size_t r2;
+    size_t c1;
+    size_t c2;
+};
 
 template<typename T>
 class Matrix {
@@ -9,6 +21,8 @@ private:
     size_t rows{};
     size_t cols{};
     std::vector<std::vector<T> > data;
+
+    void mlt_thr_add_scalar(matrix_ranges &r, Matrix<T> &res_m, T scalar, std::mutex &m);
 
 public:
     Matrix();
@@ -20,14 +34,22 @@ public:
     Matrix<T> &operator=(const Matrix<T> &e2);
 
     // matrix|scalar operations
-    Matrix<T> operator+(const T &e2);
+    Matrix<T> operator+(const T &e2) const;
 
-    Matrix<T> operator*(const T &e2);
+    Matrix<T> operator*(const T &e2) const;
+
+    Matrix<T> operator-(const T &e2) const;
+
+    Matrix<T> operator/(const T &e2) const;
 
     // matrix|matrix operations
-    Matrix<T> operator+(const Matrix<T> &e2);
+    Matrix<T> operator+(const Matrix<T> &e2) const;
 
-    Matrix<T> operator*(const Matrix<T> &e2);
+    Matrix<T> operator*(const Matrix<T> &e2) const;
+
+    Matrix<T> operator-(const Matrix<T> &e2) const;
+
+    Matrix<T> operator/(const Matrix<T> &e2) const;
 
     // access operators
     [[nodiscard]] size_t get_rows() const;
@@ -38,9 +60,15 @@ public:
 
     const T &operator()(const size_t &row, const size_t &col) const;
 
-    void print();
+    void print() const;
 
+    Matrix<T> sum(int8_t axis) const;
+
+    T sum() const;
+
+    void apply(T (*f)(T));
 };
+
 
 #include "linalg.cpp"
 
--- src/linalg.cpp
+++ src/linalg.cpp
@@ -1,9 +1,8 @@
 #ifndef LINALG_CPP
 #define LINALG_CPP
 
-#include <ostream>
-#include <iostream>
 #include "linalg.h"
+
 
 template<typename T>
 Matrix<T>::Matrix(size_t rows_num, size_t cols_num, const T &_initial_val) {
@@ -52,33 +51,82 @@ Matrix<T> &Matrix<T>::operator=(const Matrix<T> &e2) {
     return *this;
 }
 
-
 template<typename T>
-Matrix<T> Matrix<T>::operator+(const T &e2) {
-    Matrix res(rows, cols, 0.0);
+void Matrix<T>::mlt_thr_add_scalar(matrix_ranges &r, Matrix<T> &res_m, T scalar, std::mutex &m) {
+    std::cout << "\n" << r.r1 << " " << r.r2 << std::endl;
+    std::cout << r.c1 << " " << r.c2 << std::endl;
 
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < cols; j++) {
-            res(i, j) += data[i][j] + e2;
+    for (size_t i = r.r1; i < r.r2; i++) {
+        for (size_t j = r.c1; j < r.c2; j++) {
+            res_m(i, j) = data[i][j] + scalar;
         }
     }
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const T &scalar) const {
+    Matrix res(rows, cols, 0.0);
+
+    std::mutex m;
+    std::vector<matrix_ranges> ranges(THREADS_NUM);
+    std::vector<std::thread> Threads;
+
+    size_t full_slice_size = (int) (this->get_rows() / THREADS_NUM);
+    size_t remainder = (int) (this->get_rows() % THREADS_NUM);
+
+
+    for (int i = 0; i < THREADS_NUM; i++) {
+        ranges[i] = {i * full_slice_size, (i + 1) * full_slice_size + ((i == THREADS_NUM) ? remainder : 0),
+                     0, this->get_rows()};
+        Threads.emplace_back(&Matrix<T>::mlt_thr_add_scalar, this, std::ref(ranges[i]), std::ref(res), scalar,
+                             std::ref(m));
+    }
+
+    for (auto &t: Threads) { t.join(); }
 
     return res;
 }
 
 
 template<typename T>
-Matrix<T> Matrix<T>::operator*(const T &e2) {
+Matrix<T> Matrix<T>::operator*(const T &e2) const {
     Matrix res(rows, cols, 0.0);
 
     for (size_t i = 0; i < rows; i++) {
         for (size_t j = 0; j < cols; j++) {
-            res(i, j) += data[i][j] * e2;
+            res(i, j) = data[i][j] * e2;
         }
     }
 
     return res;
 }
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(const T &e2) const {
+    Matrix res(rows, cols, 0.0);
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            res(i, j) = data[i][j] - e2;
+        }
+    }
+
+    return res;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator/(const T &e2) const {
+    Matrix res(rows, cols, 0.0);
+
+    for (size_t i = 0; i < rows; i++) {
+        for (size_t j = 0; j < cols; j++) {
+            res(i, j) += data[i][j] / e2;
+        }
+    }
+
+    return res;
+}
+
 
 template<typename T>
 size_t Matrix<T>::get_rows() const {
@@ -102,35 +150,9 @@ const T &Matrix<T>::operator()(const size_t &row, const size_t &col) const {
 }
 
 template<typename T>
-Matrix<T> Matrix<T>::operator*(const Matrix<T> &m2) {
-//    when we multiplies two matrices smaller have to be rhs
-    Matrix<T> res(this->get_rows(), this->get_rows(), 0.0);
-
-    size_t m2_rows = m2.get_rows();
-    size_t m2_cols = m2.get_cols();
-
-    for (size_t i = 0; i < this->get_rows(); i++) {
-        for (size_t j = 0; j < this->get_cols(); j++) {
-            res(i, j) += data[i][j] * m2(i % m2_rows, j % m2_cols);
-        }
-    }
-    return res;
-}
-
-template<typename T>
-void Matrix<T>::print() {
-    for (int i = 0; i < this->get_rows(); i++) {
-        for (int j = 0; j < this->get_cols(); j++) {
-            std::cout << data[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-}
-
-template<typename T>
-Matrix<T> Matrix<T>::operator+(const Matrix<T> &m2) {
+Matrix<T> Matrix<T>::operator+(const Matrix<T> &m2) const {
 //    when we adding two matrices smaller have to be rhs
-    Matrix<T> res(this->get_rows(), this->get_rows(), 0.0);
+    Matrix<T> res(this->get_rows(), this->get_cols(), 0.0);
 
     size_t m2_rows = m2.get_rows();
     size_t m2_cols = m2.get_cols();
@@ -144,6 +166,64 @@ Matrix<T> Matrix<T>::operator+(const Matrix<T> &m2) {
 }
 
 template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> &m2) const {
+//    when we multiplies two matrices smaller have to be rhs
+    Matrix<T> res(this->get_rows(), this->get_cols(), 0.0);
+
+    size_t m2_rows = m2.get_rows();
+    size_t m2_cols = m2.get_cols();
+
+    for (size_t i = 0; i < this->get_rows(); i++) {
+        for (size_t j = 0; j < this->get_cols(); j++) {
+            res(i, j) += data[i][j] * m2(i % m2_rows, j % m2_cols);
+        }
+    }
+    return res;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(const Matrix<T> &m2) const {
+//    when we adding two matrices smaller have to be rhs
+    Matrix<T> res(this->get_rows(), this->get_cols(), 0.0);
+
+    size_t m2_rows = m2.get_rows();
+    size_t m2_cols = m2.get_cols();
+
+    for (size_t i = 0; i < this->get_rows(); i++) {
+        for (size_t j = 0; j < this->get_cols(); j++) {
+            res(i, j) += data[i][j] - m2(i % m2_rows, j % m2_cols);
+        }
+    }
+    return res;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator/(const Matrix<T> &m2) const {
+//    when we adding two matrices smaller have to be rhs
+    Matrix<T> res(this->get_rows(), this->get_cols(), 0.0);
+
+    size_t m2_rows = m2.get_rows();
+    size_t m2_cols = m2.get_cols();
+
+    for (size_t i = 0; i < this->get_rows(); i++) {
+        for (size_t j = 0; j < this->get_cols(); j++) {
+            res(i, j) += data[i][j] / m2(i % m2_rows, j % m2_cols);
+        }
+    }
+    return res;
+}
+
+template<typename T>
+void Matrix<T>::print() const {
+    for (int i = 0; i < this->get_rows(); i++) {
+        for (int j = 0; j < this->get_cols(); j++) {
+            std::cout << data[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+template<typename T>
 Matrix<T>::Matrix() {
     rows = 0;
     cols = 0;
@@ -152,7 +232,56 @@ Matrix<T>::Matrix() {
 }
 
 template<typename T>
-Matrix<T> transpose(Matrix<T> &m) {
+Matrix<T> Matrix<T>::sum(int8_t axis) const {
+    if (axis != 0 && axis != 1) {
+        throw std::logic_error("Invalid axis value");
+    }
+
+    size_t m_rows = this->get_rows();
+    size_t m_cols = this->get_cols();
+
+    Matrix<double> output;
+    if (axis == 0) {
+        output = *new Matrix<double>(1, m_cols, 0);
+        for (int j = 0; j < m_cols; ++j) {
+            for (int i = 0; i < m_rows; ++i)
+                output(0, j) += data[i][j];
+        }
+    } else {
+        output = *new Matrix<double>(m_rows, 1, 0);
+        for (int i = 0; i < m_rows; ++i) {
+            for (int j = 0; j < m_cols; ++j)
+                output(i, 0) += data[i][j];
+        }
+    }
+
+    return output;
+}
+
+template<typename T>
+T Matrix<T>::sum() const {
+    T res = 0;
+    size_t m_rows = this->get_rows();
+    size_t m_cols = this->get_cols();
+    for (int i = 0; i < m_rows; ++i) {
+        for (int j = 0; j < m_cols; ++j)
+            res += data[i][j];
+    }
+    return res;
+}
+
+template<typename T>
+void Matrix<T>::apply(T (*f)(T)) {
+    for (size_t i = 0; i < this->get_rows(); i++) {
+        for (size_t j = 0; j < this->get_cols(); j++) {
+            data[i][j] = (*f)(data[i][j]);
+        }
+    }
+}
+
+
+template<typename T>
+Matrix<T> transpose(const Matrix<T> &m) {
     size_t row_num = m.get_rows();
     size_t col_num = m.get_cols();
 
@@ -167,7 +296,7 @@ Matrix<T> transpose(Matrix<T> &m) {
 }
 
 template<typename T>
-Matrix<T> dot(Matrix<T> &m1, Matrix<T> &m2) {
+Matrix<T> dot(const Matrix<T> &m1, const Matrix<T> &m2) {
     if (m1.get_cols() != m2.get_rows()) { exit(1); }
 
     size_t new_rows_num = m1.get_rows();
