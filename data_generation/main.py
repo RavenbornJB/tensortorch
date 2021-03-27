cdbf3bc2eaf562769d49b7ec0aecd26395f9f03c
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-27 10:31:48 +0200

    Fixed examples, completed demo in main

--- data_generation/main.py
+++ data_generation/main.py
@@ -28,17 +28,18 @@ def generate_data_around_4_points(p1, p2, p3, p4, num_pts, sd):
             class2.append((x, y))
     return class1, class2
 
+
 if __name__ == '__main__':
-    num_points = 500
+    num_points = 100
     # g1, g2 = generate_data_around_line(1.5, 0, -1, 1, num_points, 0.5)
     g1, g2 = generate_data_around_4_points((-1, -1), (1, 1), (1, -1), (-1, 1), num_points, 0.5)
-    # with open("data_clumped_4_point_test.txt", mode="w") as f:
-    #     f.write(str(num_points))
-    #     for point in g1:
-    #         f.write("\n{0:.3f} {1:.3f}".format(point[0], point[1]))
-    #     for point in g2:
-    #         f.write("\n{0:.3f} {1:.3f}".format(point[0], point[1]))
+    with open("data_noisy_4_point_test.txt", mode="w") as f:
+        f.write(str(num_points * 2))
+        for point in g1:
+            f.write("\n{0:.3f} {1:.3f}".format(point[0], point[1]))
+        for point in g2:
+            f.write("\n{0:.3f} {1:.3f}".format(point[0], point[1]))
 
-    plt.scatter([pt[0] for pt in g1], [pt[1] for pt in g1], color="red")
-    plt.scatter([pt[0] for pt in g2], [pt[1] for pt in g2], color="green")
-    plt.show()
+    # plt.scatter([pt[0] for pt in g1], [pt[1] for pt in g1], color="red")
+    # plt.scatter([pt[0] for pt in g2], [pt[1] for pt in g2], color="green")
+    # plt.show()
--- src/main.cpp
+++ src/main.cpp
@@ -55,24 +55,70 @@ double compare(const mdb &prediction, const mdb &Y) {
 
 
 int main() {
+
+
     // DEMO 1
 
-    auto train_data = get_data("data_generation/data_linear.txt");
-    mdb X_train_pts(train_data.first);
-    mdb Y_train_pts(train_data.second);
-    auto test_data = get_data("data_generation/data_linear_test.txt");
-    mdb X_test_pts(test_data.first);
-    mdb Y_test_pts(test_data.second);
+//    auto train_data = get_data("data_generation/data_linear.txt");
+//    mdb X_train_pts(train_data.first);
+//    mdb Y_train_pts(train_data.second);
+//    auto test_data = get_data("data_generation/data_linear_test.txt");
+//    mdb X_test_pts(test_data.first);
+//    mdb Y_test_pts(test_data.second);
+//
+//    def_layers_vector lin_reg_layers = {{1, "sigmoid"}};
+//    Model linear_regression(2, lin_reg_layers, 0.01);
+//
+//    linear_regression.fit(X_train_pts, Y_train_pts, 4000, false);
+//
+//    mdb train_prediction = linear_regression.predict(X_train_pts);
+//    mdb test_prediction = linear_regression.predict(X_test_pts);
+//
+//    std::cout.precision(5);
+//    std::cout << "Accuracy on the train set: " << compare(train_prediction, Y_train_pts) * 100 << "%" << std::endl;
+//    std::cout << "Accuracy on the test set: " << compare(test_prediction, Y_test_pts) * 100 << "%" << std::endl;
 
-    def_layers_vector lin_reg_layers = {{1, "sigmoid"}};
-    Model linear_regression(2, lin_reg_layers, 0.01);
 
-    linear_regression.fit(X_train_pts, Y_train_pts, 4000, false);
+    // DEMO 2
 
-    mdb train_prediction = linear_regression.predict(X_train_pts);
-    mdb test_prediction = linear_regression.predict(X_test_pts);
+//    auto train_data = get_data("data_generation/data_clumped_4_point.txt");
+//    mdb X_train_pts(train_data.first);
+//    mdb Y_train_pts(train_data.second);
+//    auto test_data = get_data("data_generation/data_clumped_4_point_test.txt");
+//    mdb X_test_pts(test_data.first);
+//    mdb Y_test_pts(test_data.second);
+//
+//    def_layers_vector shallow_layers = {{3, "relu"}, {1, "sigmoid"}};
+//    Model shallow_model(2, shallow_layers, 0.01);
+//
+//    shallow_model.fit(X_train_pts, Y_train_pts, 10000, false);
+//
+//    mdb train_prediction = shallow_model.predict(X_train_pts);
+//    mdb test_prediction = shallow_model.predict(X_test_pts);
+//
+//    std::cout.precision(5);
+//    std::cout << "Accuracy on the train set: " << compare(train_prediction, Y_train_pts) * 100 << "%" << std::endl;
+//    std::cout << "Accuracy on the test set: " << compare(test_prediction, Y_test_pts) * 100 << "%" << std::endl;
 
-    std::cout.precision(5);
-    std::cout << "Accuracy on the train set: " << compare(train_prediction, Y_train_pts) * 100 << "%" << std::endl;
-    std::cout << "Accuracy on the test set: " << compare(test_prediction, Y_test_pts) * 100 << "%" << std::endl;
+
+    // DEMO 3
+
+//    auto train_data = get_data("data_generation/data_noisy_4_point.txt");
+//    mdb X_train_pts(train_data.first);
+//    mdb Y_train_pts(train_data.second);
+//    auto test_data = get_data("data_generation/data_noisy_4_point_test.txt");
+//    mdb X_test_pts(test_data.first);
+//    mdb Y_test_pts(test_data.second);
+//
+//    def_layers_vector deep_layers = {{3, "relu"}, {2, "relu"}, {1, "sigmoid"}};
+//    Model deep_model(2, deep_layers, 0.01);
+//
+//    deep_model.fit(X_train_pts, Y_train_pts, 20000, false);
+//
+//    mdb train_prediction = deep_model.predict(X_train_pts);
+//    mdb test_prediction = deep_model.predict(X_test_pts);
+//
+//    std::cout.precision(5);
+//    std::cout << "Accuracy on the train set: " << compare(train_prediction, Y_train_pts) * 100 << "%" << std::endl;
+//    std::cout << "Accuracy on the test set: " << compare(test_prediction, Y_test_pts) * 100 << "%" << std::endl;
 }
