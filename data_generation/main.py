1eba70bbf18a41fd9767406fb0433eac708f0fe1
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-27 02:30:30 +0200

    Added data for presentation demo

--- data_generation/main.py
+++ data_generation/main.py
@@ -7,18 +7,19 @@ def generate_data_around_line(m, b, start, end, num_pts, sd):
     below_line = []
     for _ in range(num_pts):
         x1 = random.random() * (end - start) + start
-        noise1 = random.gauss(b + 2 * sd, sd)
+        noise1 = random.gauss(b + 3 * sd, sd)
         x2 = random.random() * (end - start) + start
-        noise2 = random.gauss(b - 2 * sd, sd)
+        noise2 = random.gauss(b - 3 * sd, sd)
         above_line.append((x1, m * x1 + noise1))
         below_line.append((x2, m * x2 + noise2))
     return above_line, below_line
 
 
 if __name__ == '__main__':
-    g1, g2 = generate_data_around_line(3, 1, 5, 10, 200, 1.5)
-    with open("data_linear.txt", mode="w") as f:
-        f.write(str(200))
+    num_points = 100
+    g1, g2 = generate_data_around_line(1.5, 0, -1, 1, num_points, 0.5)
+    with open("data_linear_test.txt", mode="w") as f:
+        f.write(str(num_points))
         for point in g1:
             f.write("\n{0:.3f} {1:.3f}".format(point[0], point[1]))
         for point in g2:
